b850bccbb50f9eda4ac4845066cf5efefb8c5438
Ron Propri <rpropri@gmail.com>
2019-05-30 12:17:16 -0600

    add other components to proxy

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const proxy = require('http-proxy-middleware');
 
-const apiProxy = proxy('https://localhost:3000');
+const apiProxy = proxy('http://localhost:3000');
 
 const app = express();
 const port = 3000;
